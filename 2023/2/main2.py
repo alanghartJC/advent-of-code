2af26a0482fb98603c1d752c4171ed9a505d0693
Alex Langhart <alex.langhart@jumpcloud.com>
2023-12-02 12:08:43 -0700

    Solve Problem 2 part 2

--- 2023/2/main2.py
+++ 2023/2/main2.py
@@ -1,5 +1,10 @@
 from typing import Tuple
+from collections import defaultdict
+import functools
+import operator
+import pprint
 import re
+
 
 MAXES = {
   "red": 12,
@@ -9,24 +14,26 @@ MAXES = {
 
 game_re = re.compile(r'Game ([0-9]+): (.*) *')
 
-def solve_line(line: str) -> Tuple[bool, int]:
+def solve_line(line: str) -> int:
+  print(f"line:{line}")
   match = game_re.search(line).groups()
   game_number = int(match[0])
   game_results = match[1]
 
+  highest = defaultdict(int)
   for sample_set in game_results.split(';'):
     for (count, color) in [sample.split() for sample in sample_set.split(", ")]:
-      if int(count) > MAXES[color]:
-        return False, game_number
+      highest[color] = max(highest[color], int(count))
 
-  return True, game_number
+  game_power = functools.reduce(operator.mul, highest.values())
+  print(f'highest:{pprint.pformat(highest)}')
+  print(f"power = {game_power}")
+  return game_power
 
 def solve(inputstr: str) -> int:
   output = 0
   for line in inputstr.splitlines():
-    possible, game_number = solve_line(line)
-    if possible:
-      output += game_number
+    output += solve_line(line)
   return output
 
 def main():
