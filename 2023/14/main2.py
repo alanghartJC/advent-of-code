954828039b61a47165a244971297ce14199cf007
Alex Langhart <alex.langhart@jumpcloud.com>
2023-12-13 23:33:40 -0700

    Solve part 2

--- 2023/14/main2.py
+++ 2023/14/main2.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+def rotate_left(np_grid: np.ndarray) -> np.ndarray:
+  return np.flipud(np_grid.transpose())
+
+def rotate_right(np_grid: np.ndarray) -> np.ndarray:
+  return np.fliplr(np_grid.transpose())
+
 def calc_load(np_grid: np.ndarray) -> int:
   output = 0
   for r, row in enumerate(np_grid):
@@ -19,16 +25,42 @@ def tilt(np_grid: np.ndarray):
       elif col[row_idx] == '#':
         copy_index = row_idx+1
 
-
 def solve(input_str: str):
+  print("")
   grid_raw = []
   for line in input_str.splitlines():
     grid_raw.append(list(line))
 
   np_grid = np.array(grid_raw)
-  print(np_grid)
-  tilt(np_grid)
-  print(np_grid)
+  # print(np_grid)
+  cycles = 1000000000
+  hashes = dict()
+  cycle = 0
+  first_match_cycle = None
+  repeater_size = 0
+  while cycle < cycles:
+    print(f"Cycle {cycle}/{cycles} ({100.0*cycle/cycles:.2}%)")
+    gridhash = hash(np_grid.tobytes())
+    if gridhash in hashes:
+      if first_match_cycle is None:
+        first_match_cycle = hashes[gridhash]
+      else:
+        repeater_size += 1
+        if first_match_cycle == hashes[gridhash]:
+          cycle = 0
+          cycles = (cycles-first_match_cycle) % repeater_size
+          if cycles == 0:
+            break
+      print(f"Previous hashed: {hashes[gridhash]}")
+    for _ in range(4):
+      tilt(np_grid)
+      np_grid = rotate_right(np_grid)
+    if gridhash not in hashes:
+      hashes[gridhash] = cycle
+    cycle += 1
+
+
+  # print(np_grid)
   return calc_load(np_grid)
 
 def main():
--- 2023/14/test2.py
+++ 2023/14/test2.py
@@ -3,8 +3,18 @@ import textwrap
 
 from main2 import solve
 
-def test_sample1():
+def test_sample_1():
   sample_input = textwrap.dedent("""
+    O....#....
+    O.OO#....#
+    .....##...
+    OO.#O....O
+    .O.....O#.
+    O.#..O.#.#
+    ..O..#O..O
+    .......O..
+    #....###..
+    #OO..#....
     """).strip()
 
-  assert solve(sample_input) == 6
+  assert solve(sample_input) == 64
