ee6dced41bfdc84549bff789503d0368043f53ba
Alex Langhart <alex.langhart@jumpcloud.com>
2023-12-14 23:10:24 -0700

    Solve Part 2

--- 2023/15/main2.py
+++ 2023/15/main2.py
@@ -1,5 +1,112 @@
+from dataclasses import dataclass, field
+from typing import Dict, List
+
+@dataclass
+class Lens:
+  orig_str: str
+  name: str
+  is_add: bool
+  focal: int
+  hashnum: str
+  next: "Lens" = None
+  prev: "Lens" = None
+
+@dataclass
+class Bucket:
+  id: int
+  names: Dict[str, Lens] = field(default_factory=dict)
+  first: Lens = None
+  last: Lens = None
+
+def genhash(s: str) -> int:
+  cur = 0
+  for c in s:
+    cur += ord(c)
+    cur *= 17
+    cur %= 256
+  return cur
+
+def parse_step(s: str) -> Lens:
+  focal = None
+  if s[-1] == "-":
+    is_add = False
+    name = s[:-1]
+  else:
+    is_add = True
+    name, focal = s.split("=")
+    focal = int(focal)
+  return Lens(s, name, is_add, focal, hashnum=genhash(name))
+
+
 def solve(input_str: str):
-  pass
+  steps: List[Lens] = []
+  # import pdb; pdb.set_trace()
+  for line in input_str.splitlines():
+    steps.extend([parse_step(x) for x in line.split(',')])
+
+  map: List[Bucket] = [Bucket(x) for x in range(256)]
+
+  for s in steps:
+    # import pdb; pdb.set_trace()
+    b = map[s.hashnum]
+    if s.is_add:
+      if s.name in b.names:
+        existing = b.names[s.name]
+        prev = existing.prev
+        next = existing.next
+        if b.first == existing:
+          # b.first = existing.next
+          b.first = s
+        if b.last == existing:
+          b.last = s
+        existing.prev = None
+        existing.next = None
+        if prev:
+          prev.next = s
+        if next:
+          next.prev = s
+        s.next = next
+        s.prev = prev
+      else:
+        if b.first:
+          s.next = b.first
+          b.first.prev = s
+        b.first = s
+        if b.last is None:
+          b.last = s
+      b.names[s.name] = s
+    else:
+      # Removing
+      if s.name in b.names:
+        existing = b.names[s.name]
+        prev = existing.prev
+        next = existing.next
+        existing.prev = None
+        existing.next = None
+        if prev:
+          prev.next = next
+        if next:
+          next.prev = prev
+        if b.first == existing:
+          b.first = next
+        if b.last == existing:
+          b.last = prev
+        del b.names[s.name]
+
+  output = 0
+
+  for b in map:
+    if b.first:
+      print(f"{b.id}: ", end="")
+      cur = b.last
+      slot_num = 1
+      while cur:
+        output += (1 + b.id) * slot_num * cur.focal
+        print(f"{cur.orig_str},", end="")
+        cur = cur.prev
+        slot_num += 1
+      print("")
+  return output
 
 def main():
   with open("./input.txt", "r") as f:
--- 2023/15/test2.py
+++ 2023/15/test2.py
@@ -3,8 +3,9 @@ import textwrap
 
 from main2 import solve
 
-def test_sample1():
+def test_sample():
   sample_input = textwrap.dedent("""
+    rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
     """).strip()
 
-  assert solve(sample_input) == 6
+  assert solve(sample_input) == 145
