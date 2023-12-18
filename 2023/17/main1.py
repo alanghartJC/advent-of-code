2a0fb145b281ca0ba6dd80cf810f650fa638bc37
Alex Langhart <alex.langhart@jumpcloud.com>
2023-12-17 20:45:42 -0700

    WIP

--- 2023/17/main1.py
+++ 2023/17/main1.py
@@ -2,6 +2,8 @@ from typing import List, Tuple, Dict, Set
 from dataclasses import dataclass, field
 from pprint import pprint, pformat
 import numpy as np
+import time
+from copy import deepcopy
 
 DOWN = (1,0)
 RIGHT = (0,1)
@@ -49,32 +51,66 @@ def print_path(ndgrid: np.ndarray[Node]):
     print("")
   print("")
 
-def search_path(ndgrid: np.ndarray[Node], node: Node = None, cur_loss: int = 0, min_loss: int = None, direction: Tuple[int, int] = None, straight_len: int = 1, visited: Set[Node] = None, losses: List[int] = []) -> int:
+final_losses = None
+debug = 0
+min_len = None
+
+def search_path(ndgrid: np.ndarray[Node], node: Node = None, cur_loss: int = 0, min_loss: int = None, direction: Tuple[int, int] = None, straight_len: int = 0, visited: Set[Node] = None, losses: List[int] = []) -> int:
+  global final_losses
+  global debug
+  global min_len
   if node is None:
     node = ndgrid[0][0]
     visited = set([node])
-    losses = [node.heatloss]
+    losses = [node]
     cur_loss = node.heatloss
-    # import pdb; pdb.set_trace()
-
 
   # print(f"depth={len(visited)}")
 
+  debug += 1
+  if debug % 1000 == 0:
+    print_path(ndgrid)
+
   # print(f"Visiting {node} cur_loss={cur_loss}, direction={direction}, straight_len={straight_len}")
   if node == ndgrid[-1,-1]:
+    print(f"Path found (cur_loss={cur_loss})")
+    time.sleep(1)
     # import pdb; pdb.set_trace()
     if min_loss is None or cur_loss < min_loss:
       print(f"New minimum: {cur_loss}")
       pprint(losses)
-      print(sum(losses))
+      final_losses = deepcopy(losses)
       node.is_head = True
       print_path(ndgrid)
       node.is_head = False
     # import pdb; pdb.set_trace()
     return cur_loss
 
-  # if node == ndgrid[1][3]:
-  #   import pdb; pdb.set_trace()
+  needle = [(0,0), (0,1), (0,2), (1,2), (1,3), (1,4), (1,5), (0,5), (0,6), (0,7), (0,8),
+            (1,8), (2,8), (2,9), (2,10)]
+            # (3,10)]
+            # (4,10)]
+            # (4,11)]
+            # (5,11)]
+            # (6,11)]
+            # (7,11)]
+             # (7,12)]
+  if losses == [ndgrid[r,c] for r,c in needle]:
+    print_path(ndgrid)
+    import pdb; pdb.set_trace()
+
+  if min_loss is not None:
+    min_remaining_nodes = (ndgrid.shape[0] - node.row) + (ndgrid.shape[1] - node.col)
+    if min_remaining_nodes + cur_loss > min_loss:
+      return None
+
+  # if node == ndgrid[5][11]:
+    # import pdb; pdb.set_trace()
+    # print_path(ndgrid)
+    # print(f"min_loss={min_loss}")
+    # debug += 1
+    # if debug == 8:
+    #   import pdb; pdb.set_trace()
 
   must_turn = False
   if direction is not None and straight_len == 3:
@@ -87,6 +123,7 @@ def search_path(ndgrid: np.ndarray[Node], node: Node = None, cur_loss: int = 0, 
   # all_adj = [(d, n) for (d, n) in all_adj if n is not None and n not in visited]
 
   output = None
+  changed = False
   for d, adj_node in all_adj:
     if adj_node is None:
       continue
@@ -94,6 +131,22 @@ def search_path(ndgrid: np.ndarray[Node], node: Node = None, cur_loss: int = 0, 
       continue
     if must_turn and direction == d:
       continue
+    if node.row == 0:
+      # Top row
+      if d not in [DOWN, RIGHT]:
+        continue
+    if node.row == ndgrid.shape[0]:
+      # Bottom row
+      if d not in [UP, RIGHT]:
+        continue
+    if node.col == 0:
+      # Left col
+      if d not in [DOWN, RIGHT]:
+        continue
+    if node.col == ndgrid.shape[1]:
+      # Right row
+      if d not in [DOWN, LEFT]:
+        continue
     next_cur_loss = cur_loss+adj_node.heatloss
     if min_loss is not None and next_cur_loss > min_loss:
       continue
@@ -103,31 +156,38 @@ def search_path(ndgrid: np.ndarray[Node], node: Node = None, cur_loss: int = 0, 
       next_straight_len = 1
 
     memo_key = (d, next_straight_len)
-    # if node == ndgrid[5,11]:
+    # if node == ndgrid[2,10] and adj_node == ndgrid[3,10]:
     #   import pdb; pdb.set_trace()
     if memo_key in adj_node.memo:
-      if adj_node.memo[memo_key] is None:
-        continue
-      adj_result = next_cur_loss + adj_node.memo[memo_key]
+      # if adj_node.memo[memo_key] is None:
+      #   continue
+      adj_result = cur_loss + adj_node.memo[memo_key]
       # print(f"Using memo {adj_node} ({d}, {next_straight_len}) = {adj_result}")
     else:
       visited.add(adj_node)
       adj_node.cur_direction = d
-      adj_result = search_path(ndgrid, adj_node, next_cur_loss, min_loss, d, next_straight_len, visited, losses + [adj_node.heatloss])
+      adj_result = search_path(ndgrid, adj_node, next_cur_loss, min_loss, d, next_straight_len, visited, losses + [adj_node])
       adj_node.cur_direction = None
       visited.remove(adj_node)
 
       if adj_result is None:
-        adj_node.memo[memo_key] = None
         continue
-      adj_node.memo[memo_key] = adj_result - next_cur_loss
-      print(f"{adj_node}: {adj_result - next_cur_loss}")
+        # adj_node.memo[memo_key] = None
+        # continue
+      adj_node.memo[memo_key] = adj_result - cur_loss
+      # print(f"{adj_node}: {adj_result - next_cur_loss}")
     if min_loss is None:
       min_loss = adj_result
+      changed = True
       output = adj_result
     elif adj_result < min_loss:
+      changed = True
       min_loss = adj_result
       output = adj_result
+  if changed:
+    print(f"Changed (output={output})")
+    print_path(ndgrid)
+    # time.sleep(1)
 
   return output
 
@@ -140,8 +200,15 @@ def solve(input_str: str):
 
   ndgrid: np.ndarray = np.array(grid_raw)
 
-
   output = search_path(ndgrid)
+  for node in ndgrid.flatten():
+    node.cur_direction = None
+    node.is_head = False
+
+  for node in final_losses:
+    ndgrid[node.row,node.col].cur_direction = node.cur_direction
+  pprint(final_losses)
+  print_path(ndgrid)
   import pdb; pdb.set_trace()
   return output
 
